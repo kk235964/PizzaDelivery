084566af97295f33138b01439e869583c81d33c1
unknown <kk235964@gmail.com>
2023-01-17 02:53:17 +0530

    Some files are build and project progress forwarded

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,34 @@
-console.log("Hello from app js");
+import axios from 'axios'
+import Noty from 'noty'
+
+let addToCart = document.querySelectorAll('.add-to-cart')
+let cartCounter = document.querySelector('#cartCounter')
+function updateCart(pizza) {
+    axios.post('/update-cart', pizza).then(res =>{
+        cartCounter.innerHTML = res.data.totalQty
+        new Noty ({
+            text: 'Your Item is added to cart',
+            type: 'success',
+            timeout: 1000,
+            progressBar: false,
+            
+        }).show();
+        
+    }).catch(err=>{
+        new Noty ({
+            text: 'Something went wrong !',
+            type: 'error',
+            timeout: 1000,
+            progressBar: false,
+            
+        }).show();
+
+    })
+}
+
+addToCart.forEach((btn) =>{
+    btn.addEventListener('click', (e) =>{
+        let pizza =JSON.parse( btn.dataset.pizza)
+        updateCart(pizza)
+    })
+})
--- server.js
+++ server.js
@@ -1,22 +1,61 @@
+require('dotenv').config
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
 const path = require('path');
 const PORT = process.env.PORT || 3300;
- 
+const mongoose = require('mongoose');
+const { TRUE } = require('sass');
+mongoose.set("strictQuery", false);
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoStore = require('connect-mongo')(session);
+const { collection } = require('./app/models/menu');
+
+//Database connection
+const url = 'mongodb://127.0.0.1/pizza';
+mongoose.connect(url);
+const connection = mongoose.connection;
+connection.once('open', ()=>{
+    console.log('Database is connected...')
+
+});
+
+
+//session  config
+
+app.use(session( {
+ secret: 'thisismysecret',
+ resave: true,
+ saveUninitialized: true,
+ //session store
+ store: new MongoStore({
+    mongooseConnection: connection,
+    collection: 'sessions',
+    mongoUrl: url,
+}),
+ cookie: {maxAge: 24*60*60 * 1000}
+ }));
+ app.use(flash())
+
 //Assets
 app.use(express.static('public'));
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-    // console.log('Welcome to Home route');
-    res.render('home');
-});
+// global middleware
+app.use((req,res,next)=> {
+    res.locals.session = req.session
+    next() 
+})
 
 // Set template Engine
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
+
+require('./routes/web')(app);
+
 
 
 app.listen(3300, ()=>{
